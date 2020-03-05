a48f3e836d015a0ea2960615229327aba05b96c3
rajat mehndiratta <55960938+rajatscode@users.noreply.github.com>
2020-03-04 22:22:04 -0800

    typofix: missing semicolons

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
 # environment variables used as constants by the dotfiles repo
 export BASHRC_STORED_VARS="$HOME/.bash_stored_vars" ;
 
-export BASH_PERSONAL_PROFILE="$HOME/.bash_profile"
-export GIT_PERSONAL_PROFILE="$HOME/.gitprofile"
-export TMUX_PERSONAL_PROFILE="$HOME/.tmux.profile"
-export VIM_PERSONAL_PROFILE="$HOME/.vim_profile"
+export BASH_PERSONAL_PROFILE="$HOME/.bash_profile" ;
+export GIT_PERSONAL_PROFILE="$HOME/.gitprofile" ;
+export TMUX_PERSONAL_PROFILE="$HOME/.tmux.profile" ;
+export VIM_PERSONAL_PROFILE="$HOME/.vim_profile" ;
